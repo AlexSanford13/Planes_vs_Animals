73e9574237cf6c02467b507a19cc54d5c98d2fec
Alex Sanford <alexanderleesanford@gmail.com>
2018-07-05 17:34:18 -0400

    Updated the tables so that the longer lists were dropdown.

--- js/stylish-portfolio.js
+++ js/stylish-portfolio.js
@@ -42,6 +42,7 @@
 
 })(jQuery); // End of use strict
 
+
 // Disable Google Maps scrolling
 // See http://stackoverflow.com/a/25904582/1607849
 // Disable scroll zooming and bind back the click event
